48acd0a2aedefe1492fb4d871aadf619211381e0
LIECOTI <ib6778794@gmail.com>
2026-07-31 18:02:15 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,15 @@
-add_lunch_combo omni_tucana-userdebug
-add_lunch_combo omni_tucana-eng
+# OrangeFox Build Vars
+export FOX_R12_1=1
+export OF_FLASHLIGHT_ENABLE=1
+export OF_FL_PATH1="/sys/class/leds/led:torch_0"
+export OF_FL_PATH2="/sys/class/leds/led:torch_1"
+export OF_SCREEN_H=2340
+export OF_STATUS_H=80
+export OF_STATUS_INDENT_LEFT=48
+export OF_STATUS_INDENT_RIGHT=48
+export OF_HIDE_NOTCH=1
+export OF_USE_GREEN_LED=0
+export OF_ALLOW_DISABLE_NAVBAR=0
+export OF_QUICK_BACKUP_LIST="/boot;/recovery;/data;"
+export OF_DEFAULT_KEYMASTER_VERSION="4.0"
+export OF_FORCE_PREBUILT_KERNEL=1
